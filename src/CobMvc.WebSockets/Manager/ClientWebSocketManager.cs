c46d0935e4f25c461c91127d7bd607b98d81a4db
aspark <tsui.jacky@gmail.com>
2019-06-21 20:14:45 +0800

    dd

---
+++ src/CobMvc.WebSockets/Manager/ClientWebSocketManager.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobMvc.WebSockets
+{
+
+    /// <summary>
+    /// 客户端
+    /// </summary>
+    internal class ClientWebSocketManager : WebSocketWrapper<JsonRpcResponse, JsonRpcRequest>
+    {
+        private ILoggerFactory _loggerFactory = null;
+        private Uri _url = null;
+        //private Action<JsonRpcResponse> _callback = null;
+
+        public ClientWebSocketManager(ILoggerFactory loggerFactory, Uri url) : base(loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+            _url = url;
+            Init();
+        }
+
+        public int ID { get; set; }
+
+        protected override void Init()
+        {
+            base.Init();
+
+            Task.Factory.StartNew(KeepAlive, base.Cancellation, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        private void KeepAlive()
+        {
+            Task.Delay(10000, base.Cancellation).ContinueWith(t => {
+                base.Send(JsonRpcMessages.PingRequest).ConfigureAwait(false).GetAwaiter().GetResult();
+
+                KeepAlive();
+            });
+        }
+
+        protected override async Task<WebSocket> GetWebSocket()
+        {
+            var socket = new ClientWebSocket();
+            await socket.ConnectAsync(_url, base.Cancellation);
+
+            return socket;
+        }
+
+        ConcurrentDictionary<Guid, TaskCompletionSource<JsonRpcResponse>> _sendList = new ConcurrentDictionary<Guid, TaskCompletionSource<JsonRpcResponse>>();
+        public new Task<JsonRpcResponse> Send(JsonRpcRequest response)
+        {
+            var tcs = new TaskCompletionSource<JsonRpcResponse>();
+            _sendList.TryAdd(response.ID, tcs);
+            base.Send(response);
+
+            return tcs.Task;
+        }
+
+        protected override Task OnReceiveMessage(JsonRpcResponse msg)
+        {
+            //设置完成
+            if (_sendList.TryGetValue(msg.ID, out TaskCompletionSource<JsonRpcResponse> item))
+            {
+                item.TrySetResult(msg);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+    /// <summary>
+    /// 客户端连接池
+    /// </summary>
+    internal class ClientWebSocketPool : IDisposable
+    {
+        private ILoggerFactory _loggerFactory = null;
+        private ILogger _logger = null;
+        public ClientWebSocketPool(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+            _logger = _loggerFactory.CreateLogger<ClientWebSocketPool>();
+        }
+
+        private ConcurrentDictionary<int, ClientWebSocketManager> _items = new ConcurrentDictionary<int, ClientWebSocketManager>();
+        public ClientWebSocketManager GetOrCreate(string url)
+        {
+            var uri = new Uri(url);
+            var key = uri.AbsolutePath.GetHashCode() % 10;// pool size: 10 sockets
+
+            _logger.LogDebug($"get or add socket client:{key}");
+
+            return _items.GetOrAdd(key, k => {
+                var item = new ClientWebSocketManager(_loggerFactory, uri) { ID = k };
+                item.OnDispose += Item_OnDispose;
+
+                return item;
+            });
+        }
+
+        private void Item_OnDispose(object sender, EventArgs e)
+        {
+            Dispose(sender as ClientWebSocketManager);
+        }
+
+        public void Dispose(ClientWebSocketManager item)
+        {
+            _items.TryRemove(item.ID, out _);
+            item.OnDispose -= Item_OnDispose;
+            item.Dispose();
+        }
+
+        public void Dispose()
+        {
+            foreach (var item in _items.Values)
+            {
+                Dispose(item);
+            }
+        }
+    }
+}
---
+++ src/CobMvc.WebSockets/Manager/ServerWebSocketManager.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobMvc.WebSockets
+{
+
+    /// <summary>
+    /// 服务端
+    /// </summary>
+    internal class ServerWebSocketManager : WebSocketWrapper<JsonRpcRequest, JsonRpcResponse>
+    {
+        private ILoggerFactory _loggerFactory = null;
+        private HttpContext _context = null;
+        //private Action<JsonRpcRequest> callback = null;
+
+        public ServerWebSocketManager(ILoggerFactory loggerFactory, HttpContext context) : base(loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+            _context = context;
+            Init();
+        }
+
+        protected override Task<WebSocket> GetWebSocket()
+        {
+            return _context.WebSockets.AcceptWebSocketAsync();
+        }
+
+        protected override async Task OnReceiveMessage(JsonRpcRequest msg)
+        {
+            if (msg != null)
+            {
+                if (string.Equals(msg.Method, JsonRpcMessages.PingRequest.Method, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    await base.Send(JsonRpcMessages.PongResponse);
+                    return;
+                }
+
+                //todo:invoke mvc handle
+
+                try
+                {
+                    var route = CobWebSocketContextBag.ConfigRouteData.Routers.OfType<IRouteCollection>().First();
+
+                    //var context = _context.RequestServices.GetRequiredService<IHttpContextFactory>().Create(_context.Features);
+                    var context = new FakeHttpContext(_context.Features) { RequestServices = _context.RequestServices };
+                    context.Request.Path = msg.Method;
+                    context.Request.Method = "Get";
+
+
+                    var routerContext = new RouteContext(context);
+                    await route.RouteAsync(routerContext);
+                    if (routerContext.Handler != null)
+                    {
+                        await routerContext.Handler.Invoke(context);
+
+                        string body = "";
+                        context.Response.Body.Position = 0;//???
+                        using (var sr = new StreamReader(context.Response.Body))
+                        {
+                            body = await sr.ReadToEndAsync();
+                        }
+
+                        if (context.Response.StatusCode == 200)
+                        {
+                            var res = new JsonRpcResponse() { ID = msg.ID, Result = JsonConvert.DeserializeObject(body) };
+                            foreach (var header in context.Response.Headers)
+                            {
+                                res.Properties[header.Key] = header.Value;
+                            }
+
+                            base.SendAndForget(res);//todo:编解码了多次
+
+                            return;
+                        }
+                        else
+                        {
+                            var error = JsonRpcMessages.CreateError(msg.ID, context.Response.StatusCode, ((HttpStatusCode)context.Response.StatusCode).ToString());
+                            error.Error.Data = body;
+                            base.SendAndForget(error);
+
+                            return;
+                        }
+                    }
+
+                    base.SendAndForget(JsonRpcMessages.CreateError(msg.ID, "can not route to action"));
+
+                    return;
+                }
+                catch(Exception ex)
+                {
+                    base.SendAndForget(JsonRpcMessages.CreateError(msg.ID, ex.Message));
+
+                    throw ex;
+                }
+            }
+
+            base.SendAndForget(JsonRpcMessages.CreateError("msg is empty"));
+        }
+
+        private class FakeHttpContext : DefaultHttpContext
+        {
+            private HttpResponse _response = null;
+
+            public FakeHttpContext(IFeatureCollection features) :base(features)
+            {
+                _response = new FakeHttpResponse(this);
+            }
+
+            public override HttpResponse Response => _response;
+        }
+
+        private class FakeHttpResponse : HttpResponse
+        {
+            public FakeHttpResponse(HttpContext context)
+            {
+                _context = context;
+                Body = new MemoryStream();
+            }
+
+            private HttpContext _context = null;
+            public override HttpContext HttpContext => _context;
+
+            public override int StatusCode { get; set; }
+
+            private HeaderDictionary _header = new HeaderDictionary();
+            public override IHeaderDictionary Headers => _header;
+
+            public override Stream Body { get; set; }
+            public override long? ContentLength { get; set; }
+            public override string ContentType { get; set; }
+
+            public override IResponseCookies Cookies => throw new NotImplementedException();
+
+            public override bool HasStarted => false;
+
+            public override void OnCompleted(Func<object, Task> callback, object state)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void OnStarting(Func<object, Task> callback, object state)
+            {
+                
+            }
+
+            public override void Redirect(string location, bool permanent)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 服务端链接管理
+    /// </summary>
+    internal class ServerWebSocketPool : IDisposable
+    {
+        private ILoggerFactory _loggerFactory = null;
+        private ILogger _logger = null;
+        public ServerWebSocketPool(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+            _logger = _loggerFactory.CreateLogger<ServerWebSocketPool>();
+        }
+
+        private ConcurrentDictionary<ServerWebSocketManager, bool> _items = new ConcurrentDictionary<ServerWebSocketManager, bool>();
+        public ServerWebSocketManager Enqueue(HttpContext context)
+        {
+            _logger.LogDebug($"receive websocket:{context.Request.GetDisplayUrl()}");
+
+            var item = new ServerWebSocketManager(_loggerFactory, context);
+            item.OnDispose += Item_OnDispose;
+            _items.TryAdd(item, true);
+
+            return item;
+        }
+
+        private void Item_OnDispose(object sender, EventArgs e)
+        {
+            Dispose(sender as ServerWebSocketManager);
+        }
+
+        public void Dispose(ServerWebSocketManager item)
+        {
+            _items.TryRemove(item, out _);
+            item.OnDispose -= Item_OnDispose;
+            item.Dispose();
+        }
+
+        public void Dispose()
+        {
+            foreach (var item in _items.Keys)
+            {
+                Dispose(item);
+            }
+        }
+    }
+}
